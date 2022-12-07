7c8a22a2d0ce44dd9242e0a3c15b560ba25a4275
Max-Ryujin <max.kannen@gmail.com>
2022-12-07 22:03:54 +0100

    changed to chatgpt

--- Jarvis/Jarvis.py
+++ Jarvis/Jarvis.py
@@ -5,7 +5,7 @@ import wave
 import keyboard
 import os
 import playsound
-import openai
+from chatgpt_wrapper import ChatGPT
 
 
 chunk = 1024 
@@ -19,44 +19,62 @@ smpl_rt = 44400
 seconds = 4
 filename = "path_of_file.wav"
  
-# Create an interface to PortAudio
-pa = pyaudio.PyAudio() 
-
-#load whisper model
-model = whisper.load_model("base")
-
-# read api key
-api_key = open("GPT_API_KEY", "r").read()
-
-while True:
-    frames = [] 
-    if(keyboard.read_key() == "r"):
-        print('Recording...')
-        stream = pa.open(format=sample_format, channels=chanels,
-                 rate=smpl_rt, input=True,
-                 frames_per_buffer=chunk)
-        while(keyboard.is_pressed("r")):
-            data = stream.read(chunk)
-            frames.append(data)
-        stream.stop_stream()
-        stream.close()
-        print('processing...')      
-        sf = wave.open("C:\\Users\\MaxKa\AI Playground\\Jarvis\\x.wav", 'wb')
-        sf.setnchannels(chanels)
-        sf.setsampwidth(pa.get_sample_size(sample_format))
-        sf.setframerate(smpl_rt)
-        sf.writeframes(b''.join(frames))
-        sf.close()
-        
-        #transcripe with Whisper
-        print("Transcribe...")
-        result = model.transcribe("C:\\Users\\MaxKa\AI Playground\\Jarvis\\x.wav")
-        print(result["text"])
-
-        #call gpt-3 api function
 
 
-        myoutput = gTTS(text=result["text"], lang='en', slow=False)
-        myoutput.save("C:\\Users\\MaxKa\AI Playground\\Jarvis\\output.mp3")
-        playsound.playsound('C:\\Users\\MaxKa\AI Playground\\Jarvis\\output.mp3')
+
+
+def speak(s):
+    myoutput = gTTS(text=s, lang='en', slow=False)
+    myoutput.save("C:\\Users\\MaxKa\AI Playground\\Jarvis\\output.mp3")
+    playsound.playsound('C:\\Users\\MaxKa\AI Playground\\Jarvis\\output.mp3')
+pass
+
+if __name__ == "__main__":
+
+    #create bot
+    bot = ChatGPT()
+
+    # Create an interface to PortAudio
+    pa = pyaudio.PyAudio() 
+
+    #load whisper model
+    model = whisper.load_model("base")
+
+    while True:
+        frames = [] 
+        if(keyboard.read_key() == "r"):
+
+            #recording 
+            print('Recording...')
+            stream = pa.open(format=sample_format, channels=chanels,
+                    rate=smpl_rt, input=True,
+                    frames_per_buffer=chunk)
+            while(keyboard.is_pressed("r")):
+                data = stream.read(chunk)
+                frames.append(data)
+            stream.stop_stream()
+            stream.close()
+
+            #transcripe to wav file
+
+            print('processing...')      
+            sf = wave.open("C:\\Users\\MaxKa\AI Playground\\Jarvis\\x.wav", 'wb')
+            sf.setnchannels(chanels)
+            sf.setsampwidth(pa.get_sample_size(sample_format))
+            sf.setframerate(smpl_rt)
+            sf.writeframes(b''.join(frames))
+            sf.close()
+            
+            #transcripe with Whisper
+            print("Transcribe...")
+            result = model.transcribe("C:\\Users\\MaxKa\AI Playground\\Jarvis\\x.wav")
+            print(result["text"])
+
+            #call gpt-3 api function
+            response = bot.ask(result["text"])
+
+            speak(response)
+
+
+       
 
